c84c73d966cbd7e7a2941e35a7fa5a37e05f36e5
Androz2091 <androz2091@gmail.com>
2025-07-15 17:46:54 +0200

    move to env file

--- src/readGoogleSheet.js
+++ src/readGoogleSheet.js
@@ -6,9 +6,26 @@ const dayjs = require('dayjs');
 const customParseFormat = require('dayjs/plugin/customParseFormat');
 dayjs.extend(customParseFormat);
 
-const keyFile = path.join(__dirname, '..', 'service-account.json');
+// Load environment variables
+require('dotenv').config();
+
+// Create credentials object from environment variables
+const credentials = {
+  type: process.env.GOOGLE_SERVICE_ACCOUNT_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
+};
+
 const auth = new google.auth.GoogleAuth({
-  keyFile,
+  credentials,
   scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
 });
 
